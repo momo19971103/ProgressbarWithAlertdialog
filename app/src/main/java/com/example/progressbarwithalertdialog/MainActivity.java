dbbdf619afd694b22bf75e5d6ee1eebfbb2015cd
momo19971103 <momo19971103@yahoo.com.tw>
2019-11-01 11:37:23 +0800

    11/1更新

--- app/src/main/java/com/example/progressbarwithalertdialog/MainActivity.java
+++ app/src/main/java/com/example/progressbarwithalertdialog/MainActivity.java
@@ -9,29 +9,26 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 int count=0;
-    ProgressDialogUtil progressDialogUtil;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        progressDialogUtil = new ProgressDialogUtil();
+
         final Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
+            ProgressDialogUtil progressDialogUtil;
             public void onClick(View v) {
-
-                    ProgressDialogUtil.showProgressDialog(MainActivity.this);
+                progressDialogUtil= new ProgressDialogUtil();
+                progressDialogUtil.showProgressDialog(MainActivity.this);
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        ProgressDialogUtil.dismiss(); //你想要延遲執行的程式碼
+                        progressDialogUtil.dismiss(); //你想要延遲執行的程式碼
                     }
-                }, 1000);
-
-
-
+                }, 2000);
             }
-
         });
     }
 }
